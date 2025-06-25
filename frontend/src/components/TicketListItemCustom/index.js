2e91ae158bf768511c977fee988460cdf82ee748
RobertAb19 <95838052+RobertAb19@users.noreply.github.com>
2025-06-25 01:22:40 -0500

    Update Queues new

--- frontend/src/components/TicketListItemCustom/index.js
+++ frontend/src/components/TicketListItemCustom/index.js
@@ -502,6 +502,15 @@ const TicketListItemCustom = ({ ticket }) => {
     
 
               </Typography>
+              
+              
+                <span style={{ marginTop: 4, }} className={classes.secondaryContentSecond} >
+                  {ticket?.whatsapp?.name ? <Badge className={classes.connectionTag}>{ticket?.whatsapp?.name?.toUpperCase()}</Badge> : <br></br>}
+                  {ticketUser ? <Badge style={{ backgroundColor: "#000000" }} className={classes.connectionTag}>{ticketUser}</Badge> : <br></br>}				  
+                  <Badge style={{ backgroundColor: ticket.queue?.color || "#7c7c7c" }} className={classes.connectionTag}>{ticket.queue?.name?.toUpperCase() || "SEM FILA"}</Badge>
+                </span>
+
+
 
               <Badge
                 className={classes.newMessagesCount}
